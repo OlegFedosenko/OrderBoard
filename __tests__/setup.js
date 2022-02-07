88de217a6a4d17833504099d2ecd2ca98e4731c5
Oleg Fedosenko <olegfedosenko.b@gmail.com>
2022-02-07 15:58:30 +0200

    add react navigation

---
+++ __tests__/setup.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler/jestSetup';
+jest.useFakeTimers();
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+export const mockFormDataAppend = jest.fn();
+export const mockFormData = jest.fn().mockImplementation(() => ({ append: mockFormDataAppend }));
