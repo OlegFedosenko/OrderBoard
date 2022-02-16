ff0f7a580c4075f4939ca3013267e1e50e362ef4
Oleg Fedosenko <olegfedosenko.b@gmail.com>
2022-02-16 18:50:33 +0200

    add google sign in button

--- src/components/signIn/GoogleSignInView.tsx
+++ src/components/signIn/GoogleSignInView.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Button, View } from 'react-native';
-import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import { signOutGoogle } from '../../services/googleSignIn';
+import { View } from 'react-native';
 import { signInWithGoogle } from '../../store/session';
 import { useAsyncRequest } from '../hooks/useAsyncRequest';
+import GoogleSigninButtonView from './GoogleSigninButtonView';
 
 const GoogleSignInView = () => {
-  const { makeRequest } = useAsyncRequest(signInWithGoogle);
-  const logOut = async () => {
-    await signOutGoogle();
-  };
+  const { makeRequest, loading } = useAsyncRequest(signInWithGoogle);
   const signIn = async () => {
     await makeRequest();
   };
+  const isDisabled = loading;
   return (
     <View>
-      <GoogleSigninButton onPress={signIn} />
-      <Button onPress={logOut} title={'SignOut'} />
+      <GoogleSigninButtonView onPress={signIn} disabled={isDisabled} />
     </View>
   );
 };
