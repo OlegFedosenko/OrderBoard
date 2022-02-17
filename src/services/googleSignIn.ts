ed97a4da3ae8b129d80849a2ae80938aeb7b1a3e
Oleg Fedosenko <olegfedosenko.b@gmail.com>
2022-02-17 18:33:20 +0200

    webClientId to settings

--- src/services/googleSignIn.ts
+++ src/services/googleSignIn.ts
@@ -1,15 +1,15 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import settings from '../../settings.json';
 
+const { webClientId } = settings;
 export const signInWithGoogle = () => {
   GoogleSignin.configure({
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-    webClientId: '256566704096-6v7etev9u06g3l99llps9n0mvv5nmjkv.apps.googleusercontent.com',
+    scopes: ['https://www.googleapis.com/auth/userinfo.profile'],
+    webClientId,
     offlineAccess: true,
     forceCodeForRefreshToken: true,
   });
   return GoogleSignin.signIn();
 };
 
-export const signOutGoogle = () => {
-  return GoogleSignin.signOut();
-};
+export const signOutGoogle = () => GoogleSignin.signOut();
